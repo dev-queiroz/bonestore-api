94cdd233bb92de04842cb2905d194d10d937033e
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-07-23 13:00:06 -0300

    "Updated .gitignore, index.js, and package.json files"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const swaggerDocs = require('./swagger');
 const productRoutes = require('./routes/products');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
