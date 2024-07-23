32dc5b3b958f5a1645291e370379d05c24073d11
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-07-23 09:54:08 -0300

    "Added 'IF NOT EXISTS' clause to CREATE TABLE products statement"

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./bonestore.db');
 
 db.serialize(() => {
-    db.run(`CREATE TABLE products (
+    db.run(`CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         price INTEGER,
